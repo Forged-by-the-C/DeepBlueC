a1e39109fcf3662818e79440a2fe0904cf75e54e
Ian Shaw <ianedwardshaw@gmail.com>
2019-12-21 13:41:54 -0500

    Improved logging

--- NepalEarthquakes/src/models/ga_sk_mlpnn/mlp_nn.py
+++ NepalEarthquakes/src/models/ga_sk_mlpnn/mlp_nn.py
@@ -12,8 +12,11 @@ from ga import ga
 http://drivendata.co/blog/richters-predictor-benchmark/
 '''
 
-num_iters_per_train = 1
-pop_to_cpu_factor = 5
+NUM_ITERS_PER_TRAIN = 1
+POP_TO_CPU_FACTOR = 1
+CHROMOSOME_MAX_LEN = 10
+GENE_MAX = 50
+GENE_MIN = 0
 
 #Silence sklearn convergence warnings
 def warn(*args, **kwargs):
@@ -25,25 +28,11 @@ class mlp_ga(ga):
 
     def fitness(self, chromosome_number=0):
         hidden_layer_sizes = self.trim_to_tuple(chromosome_number)
-        clf = MLPClassifier(hidden_layer_sizes, max_iter=num_iters_per_train)
+        clf = MLPClassifier(hidden_layer_sizes, max_iter=NUM_ITERS_PER_TRAIN)
         clf.fit(self.X, self.y)
         return f1_score(y_true=self.y_val, y_pred=clf.predict(self.X_val), average='micro') 
 
 class mlp(model_wrapper):
-
-    def train(self, X,y, n_jobs, hidden_layer_sizes):
-        '''
-        input X: numpy.ndarray of shape (n_smaples, n_features)
-        input y: numpy.ndarray of shape (n_samples, )
-        input n_iter: int, number of training iterations if doing a hyper parameter search
-        input cv: int, number of cross folds to trian on
-        input n_jobs: int, number of processoers to use if doing a hyper parameter seacrch
-                            -1 indicates using all processors
-        output: trained model
-        '''
-        clf = MLPClassifier(hidden_layer_sizes, max_iter=num_iters_per_train)
-        clf.fit(X, y)
-        return clf
 
     def run_ga(self, load_population=False):
         X, y = self.load_data("train")
@@ -53,36 +42,40 @@ class mlp(model_wrapper):
             print("Loading population from file")
             self.ga = mlp_ga()
             self.ga.load_csv()
-            self.ga.population_size = pop_to_cpu_factor*os.cpu_count()
+            self.ga.population_size = POP_TO_CPU_FACTOR*os.cpu_count()
             self.ga.breed()
             prev_best_fitness = self.ga.fit_df.fitness.max()
             best_params = self.ga.trim_to_tuple(0)
         else:
-            self.ga = mlp_ga(population_factor=pop_to_cpu_factor, 
-                    chromosome_max_len=10, gene_max=300, gene_min=0)
+            self.ga = mlp_ga(population_factor=POP_TO_CPU_FACTOR, 
+                    chromosome_max_len=CHROMOSOME_MAX_LEN, gene_max=GENE_MAX, gene_min=GENE_MIN)
             self.ga.gen_population()
         self.ga.X = X
         self.ga.y = y
         self.ga.X_val = X_val
         self.ga.y_val = y_val
+        gen_number = 0
+        best_gen = 0
+        train_start = time.time()
         while True:
-            gen_number = 0
-            best_gen = 0
+            print("{} :: Training {} chromosomes in Gen {}".format(time.strftime("%H:%M:%S", 
+                time.gmtime(time.time() - train_start)), self.ga.population_size, gen_number))
             best_fitness = 0
             gen_tic = time.time()
             self.ga.multiprocess_iterate_generation()
             best_fitness = max(self.ga.fit_list)
             self.ga.to_csv()
-            if prev_best_fitness == best_fitness:
-                print("#"*15 + 
-                        "No better genes found since gen {} just finished gen {}".format(
+            if prev_best_fitness >= best_fitness:
+                print(" "* 5 + "!"*5 + 
+                        " No better genes found since gen {} just finished gen {}".format(
                             best_gen, gen_number) + "#"*15)
             else:
                 best_gen = gen_number
                 prev_best_fitness = best_fitness
                 best_params = self.ga.trim_to_tuple(np.asarray(self.ga.fit_list).argmax()) 
-            print("#"*15 +"Best Model: {} Val Score: {:.4f} Time to train gen: {:.1f}".format(
-                best_params, best_fitness, time.time() - gen_tic))
+            print(" "*5  + 
+                " Best Model: {} Val Score: {:.4f} Time to train gen: {:.1f}".format(
+                best_params, prev_best_fitness, time.time() - gen_tic))
             gen_number += 1
 
 if __name__ == "__main__":
