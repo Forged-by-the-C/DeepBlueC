77fbda66d64517caba7f51850d13db788f11046b
Ian Shaw <ianedwardshaw@gmail.com>
2019-12-22 18:16:34 -0500

    last push

--- NepalEarthquakes/src/models/autosk_scripts/run_autosk_docker.sh
+++ NepalEarthquakes/src/models/autosk_scripts/run_autosk_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd ../../../
+cd ../../../../
 #docker run  -it -p 8080:8080  --mount type=bind,source="$(pwd)",target=/app mfeurer/auto-sklearn /bin/bash -c "cd app/NepalEarthquakes && pip install -e . && cd src/models && pwd && ls -a && python3 autosk_script.py" 
-docker run  -it -p 8080:8080  --mount type=bind,source="$(pwd)",target=/app mfeurer/auto-sklearn /bin/bash -c "cd app/NepalEarthquakes && pip install -e . && cd src/models && pwd && ls -a && python3 autosk_script.py" 
+docker run  -it -p 8080:8080  --mount type=bind,source="$(pwd)",target=/app mfeurer/auto-sklearn /bin/bash -c "cd app/NepalEarthquakes && pip install -e . && cd src/models/autosk_scripts && pwd && ls -a && python3 autosk_script.py" 
